8b59538510fb95ba91703216cffd6efebf22b22b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:43:19 -0700

    Agregar archivos de proyecto.

---
+++ Videojuego/Enemigo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videojuego
+{
+    class Enemigo
+    {
+        public int PosicionX { get; set; }
+        public int PosicionY { get; set; }
+
+        private string _puntosganados;
+        public string PuntosGanados {
+            get {
+                return _puntosganados;
+            }
+            set {
+                if (value.Length >= 100)
+                {
+                    _puntosganados = value;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+        public string Movimiento { get; set; }
+        public string Disparar {get; set; }
+        public string Explotar { get; set; }
+    }
+}
